e342a34f6a749264b4a766a7cdb2feb4556eb36b
Mohamed Meabed <mo.meabed@gmail.com>
2018-05-12 05:31:37 +0400

    Update

--- server.php
+++ server.php
@@ -1,16 +1,11 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/lib/TestSoap.php';
 
 ini_set("soap.wsdl_cache_enabled", 1);
 
-$siteURL = 'http' . (empty($_SERVER['HTTPS']) ? '' : 's') . '://' . $_SERVER['HTTP_HOST'] . '/example-soap-server/';
+$siteURL = 'http' . (empty($_SERVER['HTTPS']) ? '' : 's') . '://' . $_SERVER['HTTP_HOST'] . '/';
 
-
-class ZServer extends Zend\Soap\Server
-{
-}
-
-$soap = new ZServer($siteURL . 'wsdl.php');
+$soap = new Zend\Soap\Server($siteURL . 'wsdl.php');
 $soap->setClass(TestSoap::class);
 $soap->handle();
--- wsdl.php
+++ wsdl.php
@@ -1,8 +1,8 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/lib/TestSoap.php';
 
-$siteURL = 'http' . (empty($_SERVER['HTTPS']) ? '' : 's') . '://' . $_SERVER['HTTP_HOST'] . '/example-soap-server/';
+$siteURL = 'http' . (empty($_SERVER['HTTPS']) ? '' : 's') . '://' . $_SERVER['HTTP_HOST'] . '/';
 $uri = $siteURL . 'server.php';
 
 $stub = new TestSoap();
