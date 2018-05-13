d37a077968177e9f992c87c565d94f6de519cfc2
Mohamed Meabed <mo.meabed@gmail.com>
2018-05-13 16:16:29 +0400

    Update

--- server.php
+++ server.php
@@ -7,18 +7,7 @@ ini_set("soap.wsdl_cache_ttl", 600);
 
 $siteURL = 'http' . (empty($_SERVER['HTTPS']) ? '' : 's') . '://' . $_SERVER['HTTP_HOST'] . '/';
 
-$time_start = microtime(true);
 
 $soap = new Zend\Soap\Server($siteURL . 'wsdl.php');
 $soap->setClass(TestSoap::class);
-$soap->setReturnResponse(true);
-
-$response = $soap->handle();
-
-
-if (!headers_sent()) {
-    header('Content-Type: application/xml');
-}
-
-echo $response;
-exit;
+$soap->handle();
