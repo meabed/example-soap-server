5b22915999219e251a64acbf804e516f4ca4b7ce
Mohamed Meabed <mo.meabed@gmail.com>
2018-05-13 16:22:46 +0400

    Update

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
-# Example Soap server
-
+<h2># Example Soap server</h2>
+<br>
 Deployed on herokuapp:
-
+<br>
 https://whispering-meadow-99755.herokuapp.com/wsdl.php
-
+<br>
 https://whispering-meadow-99755.herokuapp.com/server.php
